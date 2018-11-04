b542399fc8cc394f9de32c0adb9e37eb82485e42
夏山 聡史 <snatsuya@mbp-15uau-046.yjoffice.local>
2018-11-04 21:05:59 +0900

    first commit

---
+++ src/main/java/com/example/accidentpropertyapi/AccidentpropertyapiApplication.java
@@ -0,0 +1,12 @@
+package com.example.accidentpropertyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccidentpropertyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccidentpropertyapiApplication.class, args);
+	}
+}
