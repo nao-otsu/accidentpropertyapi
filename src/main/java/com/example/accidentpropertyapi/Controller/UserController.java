d98bc3afe9fbb0402721b39a63ac79e97cf8da1c
夏山聡史 <satoshi@satoshi.AirPort>
2018-12-30 02:58:09 +0900

    不要な行の削除

--- src/main/java/com/example/accidentpropertyapi/Controller/UserController.java
+++ src/main/java/com/example/accidentpropertyapi/Controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -19,10 +18,7 @@ public class UserController {
 
     @RequestMapping(value = "/user",method = RequestMethod.GET)
     public List<User> userAll(){
-        List<User> userList = new ArrayList<>();
-        userList = userService.userAll();
-        return userList;
-//        return userService.userAll();
+        return userService.userAll();
     }
 
 }
