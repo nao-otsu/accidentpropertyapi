e4a2355d45e94c7fa5138171ea73708cb65a2ff0
夏山聡史 <satoshi@satoshi.AirPort>
2018-12-30 02:49:43 +0900

    型の変更

--- src/main/java/com/example/accidentpropertyapi/Model/User.java
+++ src/main/java/com/example/accidentpropertyapi/Model/User.java
@@ -2,13 +2,15 @@ package com.example.accidentpropertyapi.Model;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class User {
     private String userId;
     private int age;
     private String addUser;
-    private String addDate;
+    private LocalDateTime addDate;
     private String updateUser;
-    private String updateDate;
+    private LocalDateTime updateDate;
     private Boolean deleteFlag;
 }
